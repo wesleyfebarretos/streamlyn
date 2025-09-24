df938fb5f2f77d70c6e8bf110fdf64838655cd83
Wesley Ferreira <wesleyfebarretos@gmail.com>
2025-09-24 09:38:47 -0300

    wipt: tus protocol integration tests

--- api/src/test/java/com/streamlyn/api/integration/TusUploadControllerTest.java
+++ api/src/test/java/com/streamlyn/api/integration/TusUploadControllerTest.java
@@ -1,6 +1,8 @@
 package com.streamlyn.api.integration;
 
+import com.streamlyn.api.domain.inputs.CreateVideoUploadInput;
 import com.streamlyn.api.domain.repositories.VideoRepository;
+import com.streamlyn.api.domain.services.VideoService;
 import com.streamlyn.entities.Video;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
@@ -9,12 +11,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.io.File;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class TusUploadControllerTest extends BaseIntegrationTest {
     private final VideoRepository videoRepository;
+    private final VideoService videoService;
 
     @Nested
     class VideoIngesting {
@@ -66,26 +73,67 @@ public class TusUploadControllerTest extends BaseIntegrationTest {
                         .isEqualTo(String.format("/files/%s", videos.getFirst().getId()));
 
                 assertThat(videoRepository.count()).isEqualTo(1L);
-            } catch(Exception e) {
+            } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }
 
         @Test
-        @DisplayName("it should write upload chunks")
+        @DisplayName("it should write upload chunks until finish")
         public void writeUploadChunks() {
-            try(InputStream inputStream = getSampleVideoStream()) {
+            File file = new File(Objects.requireNonNull(getClass().getResource("/sample-video.mp4").getFile()));
+
+            String filename = "sample-video.mp4";
+            String fileType = "video/mp4";
+            String title = "SAMPLE VIDEO";
+            List<String> tags = List.of("sample video", "mp4");
+            String description = "Sample Video Description";
+
+            CreateVideoUploadInput videoInput = new CreateVideoUploadInput(
+                    title,
+                    filename,
+                    fileType,
+                    tags,
+                    description,
+                    file.length(),
+                    String.format("filename %s,filetype %s,title %s,tags %s,description %s",
+                            Base64.getEncoder().encodeToString(filename.getBytes()),
+                            Base64.getEncoder().encodeToString(fileType.getBytes()),
+                            Base64.getEncoder().encodeToString(title.getBytes()),
+                            Base64.getEncoder().encodeToString(String.join(",", tags).getBytes()),
+                            Base64.getEncoder().encodeToString(description.getBytes())
+                    )
+            );
+
+            Video video = videoService.createUpload(videoInput);
+
+            String url = "/files/" + video.getId();
+
+            try (InputStream inputStream = getSampleVideoStream()) {
                 byte[] buffer = new byte[8192];
                 int bytesRead;
-                long totalBytes = 0;
+                long offset = 0L;
 
-//                String metadata[]
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    byte[] chunk = Arrays.copyOf(buffer, bytesRead);
+                    mockMvc.perform(
+                                    put(url)
+                                            .header("Content-Type", "application/offset+octet-stream")
+                                            .header("Content-Length", bytesRead)
+                                            .header("Tus-Resumable", "1.0.0")
+                                            .header("Upload-Offset", offset)
+                                            .content(chunk)
+                            )
+                            .andDo(print())
+//                            .andExpect(status().isCreated())
+//                            .andExpect(header().exists("Location"))
+//                            .andExpect(header().exists("Tus-Resumable"))
+//                            .andExpect(header().string("Tus-Resumable", "1.0.0"))
+                            .andReturn();
 
-                while((bytesRead = inputStream.read(buffer)) != -1) {
-                    totalBytes += bytesRead;
+                    offset += bytesRead;
                 }
 
-                System.out.println(totalBytes);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
