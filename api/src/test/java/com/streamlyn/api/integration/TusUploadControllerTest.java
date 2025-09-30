c1f72f219c95651b408046579f1a3c5262c3a960
Wesley Ferreira <wesleyfebarretos@gmail.com>
2025-09-30 09:32:03 -0300

    feat: upload file with some pauses test

--- api/src/test/java/com/streamlyn/api/integration/TusUploadControllerTest.java
+++ api/src/test/java/com/streamlyn/api/integration/TusUploadControllerTest.java
@@ -13,12 +13,14 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.InputStream;
 import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.head;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -37,20 +39,24 @@ public class TusUploadControllerTest extends BaseIntegrationTest {
                 throw new IllegalStateException("sample video file not found in resources");
             }
 
-            return inputStream;
+            return new BufferedInputStream(inputStream);
+        }
+
+        private long sampleVideoBytesLen() {
+            return new File(Objects.requireNonNull(getClass().getResource("/sample-video.mp4")).getFile()).length();
         }
 
         private MvcResult createUploadRequest() {
-            try (InputStream inputStream = getSampleVideoStream()) {
-                byte[] fileBytes = inputStream.readAllBytes();
+            long fileBytesLen = sampleVideoBytesLen();
 
-                String filename = Base64.getEncoder().encodeToString("sample-video.mp4".getBytes());
-                String fileType = Base64.getEncoder().encodeToString("video/mp4".getBytes());
-                String title = Base64.getEncoder().encodeToString("SAMPLE VIDEO".getBytes());
+            String filename = Base64.getEncoder().encodeToString("sample-video.mp4".getBytes());
+            String fileType = Base64.getEncoder().encodeToString("video/mp4".getBytes());
+            String title = Base64.getEncoder().encodeToString("SAMPLE VIDEO".getBytes());
 
+            try {
                 return mockMvc.perform(
                                 post("/files")
-                                        .header("Upload-Length", fileBytes.length)
+                                        .header("Upload-Length", Long.toString(fileBytesLen))
                                         .header("Upload-Metadata", String.format(
                                                 "filename %s,filetype %s,title %s", filename, fileType, title
                                         ))
@@ -66,6 +72,31 @@ public class TusUploadControllerTest extends BaseIntegrationTest {
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
+        }
+
+        private int writeChunk(Video video, byte[] chunk, long offset) {
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Type", "application/offset+octet-stream");
+            headers.add("Content-Length", String.valueOf(chunk.length));
+            headers.add("Tus-Resumable", "1.0.0");
+            headers.add("Upload-Offset", String.valueOf(offset));
+
+            HttpEntity<byte[]> requestEntity = new HttpEntity<>(chunk, headers);
+
+            String path = "/files/" + video.getId();
+
+            ResponseEntity<Void> response = restTemplate.exchange(path, HttpMethod.PATCH, requestEntity, Void.class);
+
+            assertThat(response .getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+            assertThat(response.getHeaders().get("upload-offset")).isNotEmpty();
+
+            Optional<String> uploadOffset = Optional.ofNullable(response.getHeaders().getFirst("upload-offset"));
+
+            assertThat(uploadOffset).isPresent();
+            assertThat(uploadOffset.get()).isEqualTo(Long.toString(chunk.length + offset));
+            assertThat(response.getHeaders().getFirst("tus-resumable")).isEqualTo("1.0.0");
+
+            return chunk.length;
         }
 
         @Test
@@ -84,7 +115,7 @@ public class TusUploadControllerTest extends BaseIntegrationTest {
         @Test
         @DisplayName("it should write upload chunks until finish")
         public void writeUploadChunks() {
-            File file = new File(Objects.requireNonNull(getClass().getResource("/sample-video.mp4").getFile()));
+            long fileBytesLen = sampleVideoBytesLen();
 
             String filename = "sample-video.mp4";
             String fileType = "video/mp4";
@@ -98,7 +129,7 @@ public class TusUploadControllerTest extends BaseIntegrationTest {
                     fileType,
                     tags,
                     description,
-                    file.length(),
+                    fileBytesLen,
                     String.format("filename %s,filetype %s,title %s,tags %s,description %s",
                             Base64.getEncoder().encodeToString(filename.getBytes()),
                             Base64.getEncoder().encodeToString(fileType.getBytes()),
@@ -120,26 +151,9 @@ public class TusUploadControllerTest extends BaseIntegrationTest {
                 while ((bytesRead = inputStream.read(buffer)) != -1) {
                     byte[] chunk = Arrays.copyOf(buffer, bytesRead);
 
-                    HttpHeaders headers = new HttpHeaders();
-                    headers.add("Content-Type", "application/offset+octet-stream");
-                    headers.add("Content-Length", String.valueOf(bytesRead));
-                    headers.add("Tus-Resumable", "1.0.0");
-                    headers.add("Upload-Offset", String.valueOf(offset));
+                    int writtenBytes = writeChunk(video, chunk, offset);
 
-                    HttpEntity<byte[]> requestEntity = new HttpEntity<>(chunk, headers);
-
-                    ResponseEntity<Void> response = restTemplate.exchange(path, HttpMethod.PATCH, requestEntity, Void.class);
-
-                    assertThat(response .getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
-                    assertThat(response.getHeaders().get("upload-offset")).isNotEmpty();
-
-                    Optional<String> uploadOffset = Optional.ofNullable(response.getHeaders().getFirst("upload-offset"));
-
-                    assertThat(uploadOffset).isPresent();
-                    assertThat(uploadOffset.get()).isEqualTo(Long.toString(bytesRead + offset));
-                    assertThat(response.getHeaders().getFirst("tus-resumable")).isEqualTo("1.0.0");
-
-                    offset += bytesRead;
+                    offset += writtenBytes;
                 }
 
                 assertThat(videoRepository.findAll()).size().isEqualTo(1);
@@ -152,9 +166,61 @@ public class TusUploadControllerTest extends BaseIntegrationTest {
         @Test
         @DisplayName("it should upload a file with pauses")
         public void fileUploadWithPauses() {
-            MvcResult result = createUploadRequest();
+            createUploadRequest();
 
-            assertThat(videoRepository.findAll()).size().isEqualTo(1);
+            List<Video> videos = videoRepository.findAll();
+
+            assertThat(videos).size().isEqualTo(1);
+
+            try(InputStream inputStream = getSampleVideoStream()) {
+                byte[] buffer = new byte[8192];
+                int bytesRead;
+                long offset = 0L;
+                int uploadPauses = 4;
+                long bytesPerPause = sampleVideoBytesLen() / uploadPauses;
+                int writtenBytes = 0;
+
+                for (int i = 1; i <= uploadPauses; i++) {
+                    MvcResult result = mockMvc.perform(
+                                    head(String.format("/files/%s", videos.getFirst().getId()))
+                                            .header("Tus-Resumable", "1.0.0")
+
+                            )
+                            .andDo(print())
+                            .andExpect(status().isOk())
+                            .andExpect(header().longValue("upload-offset", offset))
+                            .andReturn();
+
+                    offset = Long.parseLong(Objects.requireNonNull(result.getResponse().getHeader("upload-offset")));
+
+                    if(i == uploadPauses) {
+                        bytesPerPause = sampleVideoBytesLen();
+                    }
+
+                    while(writtenBytes <= bytesPerPause && (bytesRead = inputStream.read(buffer)) != -1) {
+                        byte[] chunk = Arrays.copyOf(buffer, bytesRead);
+
+                        int wb = writeChunk(videos.getFirst(), chunk, offset);
+
+                        offset += wb;
+                        writtenBytes += wb;
+                    }
+
+                    writtenBytes = 0;
+                }
+
+                mockMvc.perform(
+                                head(String.format("/files/%s", videos.getFirst().getId()))
+                                        .header("Tus-Resumable", "1.0.0")
+
+                        )
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andExpect(header().longValue("upload-offset", sampleVideoBytesLen()))
+                        .andReturn();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
